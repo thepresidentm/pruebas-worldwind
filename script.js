557c241348341b0e91e48c61d6aea6054ba452b6
thepresidentm <miguelanva9@gmail.com>
2021-10-02 17:36:52 -0500

    lejania corregida

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 /* wwd.addLayer(new WorldWind.CompassLayer());*/
 /* wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd)); */
-/* wwd.addLayer(new WorldWind.ViewControlsLayer(wwd)); */
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+wwd.navigator.range = 300e5;
+/*var posicion = new.;*/
+
+
+/* wwd.KmlFile(new KmlScale(100000000)); */
